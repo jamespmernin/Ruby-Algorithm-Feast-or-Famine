4ccb0c41418902bf276ca4a4929d4ddb07ea3183
James Mernin <jamespmernin@gmail.com>
2020-11-20 16:54:24 -0500

    James Mernin - Algorithm solved

--- feast_or_famine.rb
+++ feast_or_famine.rb
@@ -3,9 +3,12 @@
 # Pseudocode here.
 
 def feast_or_famine(beast, dish)
-
+  if beast[0] == dish[0] && beast[-1] == dish[-1]
+    return beast[0] + beast[-1]
+  end
+  return false
 end
 
 # Print here.
 
-puts feast("great blue heron", "garlic naan")
+puts feast_or_famine("great blue heron", "garlic naan")
